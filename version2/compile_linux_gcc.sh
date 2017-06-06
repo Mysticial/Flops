01b69450a1b5d2664fd9941f46a28597ebf9d7cc
Alex Yee <a-yee@u.northwestern.edu>
2017-06-05 21:07:42 -0500

    Add Skylake Purley.

--- version2/compile_linux_gcc.sh
+++ version2/compile_linux_gcc.sh
@@ -3,10 +3,11 @@ mkdir "binaries-linux" #
 basepath="binaries-linux/x64-" #
 flags="-std=c++0x -static -fopenmp -O2" #
 #
-g++ source/*.c source/*.cpp -o "${basepath}Core2.out"          $flags -march=core2       -D "ArchCore2" #
-g++ source/*.c source/*.cpp -o "${basepath}SandyBridge.out"    $flags -march=sandybridge -D "ArchSandyBridge" #
-g++ source/*.c source/*.cpp -o "${basepath}Bulldozer.out"      $flags -march=bdver1      -D "ArchBulldozer" #
-g++ source/*.c source/*.cpp -o "${basepath}Piledriver.out"     $flags -march=bdver2      -D "ArchPiledriver" #
-g++ source/*.c source/*.cpp -o "${basepath}Haswell.out"        $flags -march=haswell     -D "ArchHaswell" #
-g++ source/*.c source/*.cpp -o "${basepath}KnightsLanding.out" $flags -march=knl         -D "ArchKnightsLanding" #
+g++ source/*.c source/*.cpp -o "${basepath}Core2.out"          $flags -march=core2          -D "ArchCore2" #
+g++ source/*.c source/*.cpp -o "${basepath}SandyBridge.out"    $flags -march=sandybridge    -D "ArchSandyBridge" #
+g++ source/*.c source/*.cpp -o "${basepath}Bulldozer.out"      $flags -march=bdver1         -D "ArchBulldozer" #
+g++ source/*.c source/*.cpp -o "${basepath}Piledriver.out"     $flags -march=bdver2         -D "ArchPiledriver" #
+g++ source/*.c source/*.cpp -o "${basepath}Haswell.out"        $flags -march=haswell        -D "ArchHaswell" #
+g++ source/*.c source/*.cpp -o "${basepath}KnightsLanding.out" $flags -march=knl            -D "ArchKnightsLanding" #
+g++ source/*.c source/*.cpp -o "${basepath}SkylakePurley.out"  $flags -march=skylake-avx512 -D "ArchKnightsLanding" #
 #
