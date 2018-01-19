91a4977892add62922dd6e11b500599d08594ae4
Xin Zhang <zhangxin.xa@gmail.com>
2018-01-19 11:43:19 +0800

    Fix single precision reduce

--- version3/Source/256/f32v3_Reduce_AVX.h
+++ version3/Source/256/f32v3_Reduce_AVX.h
@@ -21,7 +21,7 @@ namespace Flops{
 ////////////////////////////////////////////////////////////////////////////////
 inline float reduce(__m256 y){
     __m128 x = _mm_add_ps(_mm256_castps256_ps128(y), _mm256_extractf128_ps(y, 1));
-    x = _mm_add_ps(x, _mm_unpackhi_ps(x, x));
+    x = _mm_add_ps(x, _mm_shuffle_ps(x, x, 0b00001110));
     x = _mm_add_ps(x, _mm_shuffle_ps(x, x, 1));
     return _mm_cvtss_f32(x);
 }
